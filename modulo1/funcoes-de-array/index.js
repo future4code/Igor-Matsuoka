3a36ece0b47d3af3c59b3e1f776bfef15a7f8b25
Igor Matsuoka <igor.mtka@gmail.com>
2021-10-15 16:53:16 -0300

    exercicios-funcoes-de-array

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,267 @@
+//***********************************************************************************************************************************************************************************
+/* Exercício de Interpretação de código
+
+1.
+
+const usuarios = [
+  { nome: "Amanda Rangel", apelido: "Mandi" },
+  { nome: "Laís Petra", apelido: "Laura" },
+  { nome: "Letícia Chijo", apelido: "Chijo" }
+]
+
+const novoArrayA = usuarios.map((item, index, array) => {
+   console.log(item, index, array)
+})
+
+    R1. Será impresso: { nome: "Amanda Rangel", apelido: "Mandi" }, 0 e o array com todos os items do objeto declarado
+                       { nome: "Laís Petra", apelido: "Laura" }, 1 e o array com todos os items do objeto declarado
+                       { nome: "Letícia Chijo", apelido: "Chijo" }, 2 e o array com todos os items do objeto declarado
+
+//***********************************************************************************************************************************************************************************
+
+2.
+
+const usuarios = [
+  { nome: "Amanda Rangel", apelido: "Mandi" },
+  { nome: "Laís Petra", apelido: "Laura" },
+  { nome: "Letícia Chijo", apelido: "Chijo" },
+]
+
+const novoArrayB = usuarios.map((item, index, array) => {
+   return item.nome
+})
+
+console.log(novoArrayB)
+
+    R2.  Será impresso: ['Amanda Rangel', 'Laís Petra', 'Letícia Chijo'] e irá mostrar o index de cada um
+
+//***********************************************************************************************************************************************************************************   
+
+3.
+
+const usuarios = [
+  { nome: "Amanda Rangel", apelido: "Mandi" },
+  { nome: "Laís Petra", apelido: "Laura" },
+  { nome: "Letícia Chijo", apelido: "Chijo" },
+]
+
+const novoArrayC = usuarios.filter((item, index, array) => {
+   return item.apelido !== "Chijo"
+})
+
+console.log(novoArrayC)
+
+    R3. Será impresso: [{ nome: "Amanda Rangel", apelido: "Mandi" },{ nome: "Laís Petra", apelido: "Laura" }] e irá mostrar o index de cada um
+
+*/
+
+//***********************************************************************************************************************************************************************************
+//Exercícios de escrita
+
+//1.a)
+
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+ const arrayNomeDosPets = pets.map((item, index, array) => {
+     return item.nome
+ })
+
+ console.log(arrayNomeDosPets)
+
+//1.b)
+
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+ const arrayDogsSalsichas = pets.filter((item, index, array) => {
+    return (item.raca === "Salsicha")
+ })
+
+ console.log(arrayDogsSalsichas)
+
+ 
+
+//1.c)
+
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+const cupomParaPoodle = pets.filter((item, index)=>{
+    return (item.raca === "Poodle")
+ }) 
+const cupomParaPoodleNome = cupomParaPoodle.map((item, index)=>{
+    return `Você ganhou um cupom de desconto de 10% para tosar o/a ${item.nome}`
+})
+
+console.log (cupomParaPoodleNome) 
+
+//***********************************************************************************************************************************************************************************
+
+//2.a)
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+ const nomesDosProdutos = produtos.map((item, index, array)=>{
+     return item.nome
+ })
+
+ console.log(nomesDosProdutos)
+
+ //2.b)
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+const desconto = produtos.map((item)=>{
+    return novoObjeto = {nome: item.nome, preco: item.preco*0.95}
+}) 
+
+console.log(desconto)
+
+//2.c)
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+ const precoDosProdutos = produtos.filter((item, index, array)=>{
+    return (item.categoria === "Bebidas")
+})
+
+console.log(precoDosProdutos)
+
+//2.d)
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+ const produtosDeYpe = produtos.filter((item, index, array)=>{
+     return (item.nome.includes("Ypê"))
+ })
+
+ console.log(produtosDeYpe)
+
+ //2.e)
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+const produtosDeYpe = produtos.filter((item, index, array)=>{
+     return (item.nome.includes("Ypê"))
+ })
+
+ const compreProduto = produtosDeYpe.map((item, index, array)=>{
+    return `Compre ${item.nome} por ${item.preco}`
+ })
+
+ console.log(compreProduto)
+
+//***********************************************************************************************************************************************************************************
+
+//Desafio 1
+//a)
+
+const pokemons = [
+    { nome: "Bulbasaur", tipo: "grama" },
+    { nome: "Bellsprout", tipo: "grama" },
+    { nome: "Charmander", tipo: "fogo" },
+    { nome: "Vulpix", tipo: "fogo" },
+    { nome: "Squirtle", tipo: "água" },
+    { nome: "Psyduck", tipo: "água" },
+ ]
+
+const nomesPokemons = pokemons.map((item, index, array) => {
+    return item.nome
+ })
+
+const nomeOrdemAlfabetica = nomesPokemons.map((item, index, array) => {
+    return nomesPokemons.sort()
+})
+
+ console.log(nomesPokemons)
+
+//b)
+const pokemons = [
+    { nome: "Bulbasaur", tipo: "grama" },
+    { nome: "Bellsprout", tipo: "grama" },
+    { nome: "Charmander", tipo: "fogo" },
+    { nome: "Vulpix", tipo: "fogo" },
+    { nome: "Squirtle", tipo: "água" },
+    { nome: "Psyduck", tipo: "água" },
+ ]
+
+const tipoPokemon = pokemons.map((item, index, array) => {
+    return item.tipo
+})
+
+const novaArray = [...new Set(tipoPokemon)]
+console.log(novaArray)
