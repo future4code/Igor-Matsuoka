63e01ecfa3d51a997160f92e96f301dfa564ab31
Igor Matsuoka <igor.mtka@gmail.com>
2021-11-05 18:07:58 -0300

    react-array

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -8,32 +8,100 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const InputInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
 
-class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'Joãozinho'}
-          fotoUsuario={'https://picsum.photos/70/50'}
-          fotoPost={'https://picsum.photos/220/150'}
-        />
-
-        <Post
-          nomeUsuario={'Marquin'}
-          fotoUsuario={'https://picsum.photos/60/50'}
-          fotoPost={'https://picsum.photos/210/150'}
-        />
-
-      </MainContainer>
-    );
+class PostArray extends React.Component{
+  state = {
+    inputNomeUsuario: "",
+    inputFotoUsuario: "",
+    inputFotoPost: "",
+    pessoas: [
+      {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/20/50",
+        fotoPost: "https://picsum.photos/200/150"
+      },
+      {
+        nomeUsuario: "Joãozinho",
+        fotoUsuario: "https://picsum.photos/70/50",
+        fotoPost: "https://picsum.photos/220/150"
+      },
+      {
+        nomeUsuario: "Marquin",
+        fotoUsuario: "https://picsum.photos/60/50",
+        fotoPost: "https://picsum.photos/210/150"
+      }
+    ]
   }
+
+  guardaNomeUsuario = (event) => {
+    this.setState({ inputNomeUsuario: event.target.value });
+  };
+
+  guardaFotoUsuario = (event) => {
+    this.setState({ inputFotoUsuario: event.target.value });
+  };
+
+  guardaFotoPost = (event) => {
+    this.setState({ inputFotoPost: event.target.value });
+  };
+
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.inputNomeUsuario,
+      fotoUsuario: this.state.inputFotoUsuario,
+      fotoPost: this.state.inputFotoPost
+    };
+
+    const copia = [...this.state.pessoas, novoPost];
+
+    this.setState({pessoas: copia})
+    this.setState({inputNomeUsuario: "", inputFotoUsuario: "", inputFotoPost: "",})
+  }
+
+    render() {
+    const listaDePosts = this.state.pessoas.map((pessoa)=>{
+      return(
+        <MainContainer>
+          <Post
+          nomeUsuario = {pessoa.nomeUsuario}
+          fotoUsuario = {pessoa.fotoUsuario}
+          fotoPost = {pessoa.fotoPost}
+          />
+        </MainContainer>
+      )
+    })
+  
+      return(
+        <InputInfo>
+          <h2>Poste uma foto!</h2>
+          <input
+            value={this.state.inputNomeUsuario}
+            onChange={this.guardaNomeUsuario}
+            placeholder="Nome do usuário"
+            />
+          <input
+            value={this.state.inputFotoUsuario}
+            onChange={this.guardaFotoUsuario}
+            placeholder="URL da foto de perfil"
+            />
+          <input
+            value={this.state.inputFotoPost}
+            onChange={this.guardaFotoPost}
+            placeholder="URL do post"
+            />
+            <button onClick={this.adicionaPost}>Adicionar</button>
+          {listaDePosts}
+        </InputInfo>
+      )
+  }
+  
+
 }
 
-export default App;
+export default PostArray;
