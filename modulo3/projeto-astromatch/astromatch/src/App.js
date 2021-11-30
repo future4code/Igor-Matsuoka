fbbf820e29a427df7d17e04b0a38f611836d08fa
Igor Matsuoka <igor.mtka@gmail.com>
2021-11-30 16:32:50 -0300

    exercicio

---
+++ modulo3/projeto-astromatch/astromatch/src/App.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+/* import DisplayTelaInicial from './components/DisplayTelaInicial';
+import TelaMatches from './components/TelaMatches'; */
+
+const AppContainer = styled.div `
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+function App(){
+  return(
+    <AppContainer>
+    </AppContainer>
+  )
+}
+export default App
