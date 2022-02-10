d67dcae226f939aed702843b2658b1ceae691f0a
Igor Matsuoka <igor.mtka@gmail.com>
2022-02-10 19:44:13 -0300

    segundo desafio funcionando

--- modulo4/labecommerce/src/endpoints/products/getProducts.ts
+++ modulo4/labecommerce/src/endpoints/products/getProducts.ts
@@ -2,12 +2,63 @@ import { Request, Response } from "express";
 import { connection } from "../../data/connection"
 import { Product } from "../../types/types";
 
+async function sortProductsDESC():Promise<any> {
+    const result = await connection.raw(`
+       SELECT id, name, price, image_url
+       FROM labecommerce_products 
+       ORDER BY price DESC
+    `)
+ 
+    return result[0]
+}
+
+async function sortProductsASC():Promise<any> {
+    const result = await connection.raw(`
+       SELECT id, name, price, image_url
+       FROM labecommerce_products 
+       ORDER BY price ASC
+    `)
+ 
+    return result[0]
+}
+
+
+async function searchProducts(name:string):Promise<any> {
+    const result = await connection.raw(`
+       SELECT id, name, price, image_url
+       FROM labecommerce_products 
+       WHERE name like '%${name}%'
+    `)
+ 
+    return result[0]
+}
+
 export const getProducts = async (req: Request, res: Response):Promise<void> => {
     try {
-        const products:Product[] = await connection("labecommerce_products")
-        .select()
+        const order = req.query.order
+        const search = req.query.search as string
 
-        res.status(201).send({products: products})
+        if (search && !order) {
+            const result:Product[] = await searchProducts(search)
+            res.send({products: result})
+            
+        } else if (order && order === "asc" && !search) {
+            const result:Product[] = await sortProductsASC()
+            res.send({products: result})
+
+        } else if(order && order === "desc" && !search) {
+            const result:Product[] = await sortProductsDESC()
+            res.send({products: result})
+
+        } else if (!search || !order) {
+            const products:Product[] = await connection("labecommerce_products")
+            .select()
+            res.status(201).send({products: products})
+
+        } else {
+            throw new Error("Order needs to be a valid parameter")
+        }
+        
     } catch (error:any) {
         res.send({error, message:error.message})
     }
