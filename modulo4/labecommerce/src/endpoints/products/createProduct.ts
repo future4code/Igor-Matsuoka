85c1e452a33de2a30ee7b72ef6f08438f581b4c5
Igor Matsuoka <igor.mtka@gmail.com>
2022-02-11 17:41:46 -0300

    adicionando verificações createProduct

--- modulo4/labecommerce/src/endpoints/products/createProduct.ts
+++ modulo4/labecommerce/src/endpoints/products/createProduct.ts
@@ -3,15 +3,21 @@ import { connection } from "../../data/connection"
 
 export const createProduct = async (req: Request, res: Response):Promise<void> => {
     try {
+        let errorCode = 400
         const {name, price, image_url} = req.body
 
-        await connection("labecommerce_products")
-        .insert({
+        if(!name || !price || !image_url){
+            errorCode = 422
+            throw new Error("Parameter is missing")
+        } else {
+            await connection("labecommerce_products")
+            .insert({
             id: Date.now().toString(),
             name,
             price,
             image_url
-        })
+            })
+        }
 
         res.status(201).send({message: "Produto cadastrado com sucesso!"})
     } catch (error:any) {
