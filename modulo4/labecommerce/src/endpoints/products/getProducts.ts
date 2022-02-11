158a1f1bb3df7ee7a885c0f0d00683755042de05
Igor Matsuoka <igor.mtka@gmail.com>
2022-02-11 17:48:04 -0300

    adicionando mais verificações

--- modulo4/labecommerce/src/endpoints/products/getProducts.ts
+++ modulo4/labecommerce/src/endpoints/products/getProducts.ts
@@ -35,30 +35,48 @@ async function searchProducts(name:string):Promise<any> {
 
 export const getProducts = async (req: Request, res: Response):Promise<void> => {
     try {
+        let errorCode = 400
         const order = req.query.order
         const search = req.query.search as string
 
         if (search && !order) {
             const result:Product[] = await searchProducts(search)
             res.send({products: result})
+
+            if(result.length === 0){
+                throw new Error("There is no products")
+            }
             
         } else if (order && order === "asc" && !search) {
             const result:Product[] = await sortProductsASC()
             res.send({products: result})
 
+            if(result.length === 0){
+                throw new Error("There is no products")
+            }
+
         } else if(order && order === "desc" && !search) {
             const result:Product[] = await sortProductsDESC()
             res.send({products: result})
+
+            if(result.length === 0){
+                throw new Error("There is no products")
+            }
 
         } else if (!search || !order) {
             const products:Product[] = await connection("labecommerce_products")
             .select()
             res.status(201).send({products: products})
 
+            if(products.length === 0){
+                throw new Error("There is no products")
+            }
+
         } else {
+            errorCode = 422
             throw new Error("Order needs to be a valid parameter")
         }
-        
+
     } catch (error:any) {
         res.send({error, message:error.message})
     }
