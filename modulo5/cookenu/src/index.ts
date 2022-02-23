0a455eb41f06fd9870184313aa5b1ea2f9750935
Igor Matsuoka <igor.mtka@gmail.com>
2022-02-23 18:17:07 -0300

    quarto endpoint criado

--- modulo5/cookenu/src/index.ts
+++ modulo5/cookenu/src/index.ts
@@ -3,9 +3,12 @@ import { login } from "./Endpoints/login";
 import { getOwnProfile } from "./Endpoints/getOwnProfile";
 import { signUp } from "./Endpoints/signUp";
 import { getUserProfile } from "./Endpoints/getProfile";
+import { createRecipe } from "./Endpoints/createRecipe";
+
 
 app.post("/signup", signUp)
 app.post("/login", login)
+app.post("/recipe", createRecipe)
 
 app.get("/user/profile", getOwnProfile)
 app.get("/user/:id", getUserProfile)
