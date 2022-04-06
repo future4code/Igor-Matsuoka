ab3ddbd12c3e492449a2bb81cc9834dcb29fab72
Igor Matsuoka <igor.mtka@gmail.com>
2022-04-06 18:08:48 -0300

    cors

--- modulo6/cubo/back-end/src/Data/app.ts
+++ modulo6/cubo/back-end/src/Data/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { AddressInfo } from "net";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 export const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
